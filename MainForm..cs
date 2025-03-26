9919063f65108fc1d8d42047aa722d2dd0873eea
CheQuocDat <datdz0985@gmail.com>
2025-03-26 17:57:47 +0700

    Giai quyet xung dot khi xoa Form

--- MainForm..cs
+++ MainForm..cs
@@ -19,8 +19,8 @@ namespace ContactList
 
         private void button1_Click(object sender, EventArgs e)
         {
-            AddContactForm form2 = new AddContactForm();
-            form2.Show();
+           
+
         }
     }
 }
